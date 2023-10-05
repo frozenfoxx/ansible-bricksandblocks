6959c2132eb3fb6c07bcfd46c85e633019829f0b
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2023-10-05 14:40:46 -0700

    Added heimdall

---
+++ roles/rsync_backup/files/heimdall_backup.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# Backup a Heimdall server
+
+# Variables
+BACKUP_DATE=$(/bin/date +%Y-%m-%d)
+LOG_PATH=${LOG_PATH:-'/var/log'}
+SOURCE_PATH=${SOURCE_PATH:-'/data/heimdall'}
+SSHPASS_PATH=${SSHPASS_PATH:-"${HOME}/.sshpass"}
+TARGET_HOST=${TARGET_HOST:-''}
+TARGET_PATH=${TARGET_PATH:-':NetBackup/heimdall_backups'}
+TARGET_USER=${TARGET_USER:-'backup'}
+
+# Functions
+
+## Check prerequisite commands
+check_requirements()
+{
+  # Check for rsync
+  if ! command -v rsync &> /dev/null; then
+    echo "rsync not found!"
+    exit 1
+  fi
+
+  # Check for sshpass
+  if ! command -v sshpass &> /dev/null; then
+    echo "sshpass not found!"
+    exit 1
+  fi
+}
+
+## Clean up old backups
+cleanup_backup()
+{
+  cd /tmp && ls -tp | grep heimdall_backup | grep -v '/$' | tail -n +4 | xargs -I {} rm -- {}
+}
+
+## Create backup file
+create_backup()
+{
+  # Change to the Heimdall installation
+  cd ${SOURCE_PATH}
+
+  # Stop Heimdall
+  docker compose -f ./docker-compose.yml down
+
+  # Create archive
+  tar cfz /tmp/heimdall_backup-${BACKUP_DATE}.tar.gz ./config
+
+  # Restart Heimdall
+  docker compose -f ./docker-compose.yml up -d
+}
+
+## Sync the backup to a target and write a log
+sync_backup()
+{
+  echo "Syncing to ${TARGET_HOST}..."
+  rsync -av -e "sshpass -f ${SSHPASS_PATH} ssh" /tmp/heimdall_backup* ${TARGET_USER}@${TARGET_HOST}:${TARGET_PATH}/ > ${LOG_PATH}/heimdall_backup.log
+}
+
+## Display usage information
+usage()
+{
+  echo "Usage: [Environment Variables] heimdall_backup.sh [options]"
+  echo "  Requirements:"
+  echo "    rsync                necessary for cloning"
+  echo "    ssh                  necessary for remote shell"
+  echo "    sshpass              necessary for backup to hosts without proper SSH key support"
+  echo "  Environment Variables:"
+  echo "    SOURCE_PATH          file path to use from the source (default: '/data/heimdall')"
+  echo "    TARGET_HOST          rsync remote target host (default: '')"
+  echo "    TARGET_PATH          file path to use from the target (default: ':NetBackup/heimdall_backups')"
+  echo "    TARGET_USER          user to connect to the target with (default: 'backup')"
+  echo "  Options:"
+  echo "    -h | --help          display this usage"
+}
+
+# Logic
+
+## Argument parsing
+while [[ "$#" > 0 ]]; do
+  case $1 in
+    -h | --help ) usage
+                  exit 0
+  esac
+  shift
+done
+
+check_requirements
+create_backup
+sync_backup
+cleanup_backup
