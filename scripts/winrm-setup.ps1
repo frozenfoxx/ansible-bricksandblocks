fa4f7d86ede31ef2c5c509c7daec1771b9db91a8
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-03-27 16:55:24 -0700

    Added WinRM setup script

---
+++ scripts/winrm-setup.ps1
@@ -0,0 +1,63 @@
+# Source: https://docs.ansible.com/ansible/latest/os_guide/windows_winrm.html#winrm-setup
+
+# Enables the WinRM service and sets up the HTTP listener
+Enable-PSRemoting -Force
+
+# Opens port 5985 for all profiles
+$firewallParams = @{
+    Action      = 'Allow'
+    Description = 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5985]'
+    Direction   = 'Inbound'
+    DisplayName = 'Windows Remote Management (HTTP-In)'
+    LocalPort   = 5985
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+}
+New-NetFirewallRule @firewallParams
+
+# Allows local user accounts to be used with WinRM
+# This can be ignored if using domain accounts
+$tokenFilterParams = @{
+    Path         = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+    Name         = 'LocalAccountTokenFilterPolicy'
+    Value        = 1
+    PropertyType = 'DWORD'
+    Force        = $true
+}
+New-ItemProperty @tokenFilterParams
+
+# Create self signed certificate
+$certParams = @{
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    DnsName           = $env:COMPUTERNAME
+    NotAfter          = (Get-Date).AddYears(1)
+    Provider          = 'Microsoft Software Key Storage Provider'
+    Subject           = "CN=$env:COMPUTERNAME"
+}
+$cert = New-SelfSignedCertificate @certParams
+
+# Create HTTPS listener
+$httpsParams = @{
+    ResourceURI = 'winrm/config/listener'
+    SelectorSet = @{
+        Transport = "HTTPS"
+        Address   = "*"
+    }
+    ValueSet = @{
+        CertificateThumbprint = $cert.Thumbprint
+        Enabled               = $true
+    }
+}
+New-WSManInstance @httpsParams
+
+# Opens port 5986 for all profiles
+$firewallParams = @{
+    Action      = 'Allow'
+    Description = 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]'
+    Direction   = 'Inbound'
+    DisplayName = 'Windows Remote Management (HTTPS-In)'
+    LocalPort   = 5986
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+}
+New-NetFirewallRule @firewallParams
